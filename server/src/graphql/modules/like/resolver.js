50b8f68727c5cb74d588e23e5c75417b257231be
MateusOli21 <mateus.oli.abreu@hotmail.com>
2020-12-17 18:38:51 -0300

    Created add and delete comment mutation, comments counter, like post and likes counter for posts.

---
+++ server/src/graphql/modules/like/resolver.js
@@ -0,0 +1,30 @@
+const { UserInputError } = require('apollo-server');
+
+const checkAuth = require('../../../util/checkAuth');
+const Post = require('../../../models/Post');
+
+module.exports = {
+  Mutation: {
+    likePost: async (_, { postId }, context) => {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (!post) throw new UserInputError('Post not found.');
+
+      let response = '';
+
+      if (post.likes.find((like) => like.username === username)) {
+        post.likes = post.likes.filter((like) => like.username !== username);
+        response = 'UnLiked post.';
+      } else {
+        post.likes.push({ username, createdAt: new Date().toISOString() });
+        response = 'Liked post.';
+      }
+
+      await post.save();
+
+      return response;
+    },
+  },
+};
--- server/src/graphql/modules/posts/resolver.js
+++ server/src/graphql/modules/posts/resolver.js
@@ -58,4 +58,8 @@ module.exports = {
       }
     },
   },
+  Post: {
+    likesCount: (parent) => parent.likes.length,
+    commentsCount: (parent) => parent.comments.length,
+  },
 };
